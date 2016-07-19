56d292b400ea78b2a5c016dd011341dbeaa1fbcb
Nikiforov Alexander <nikiforo.alexander@gmail.com>
2016-07-19 20:35:23 +0200

    added `addNewRole` method with POST request to save new Role

--- src/main/java/com/techdegree/instateam/web/controller/RoleController.java
+++ src/main/java/com/techdegree/instateam/web/controller/RoleController.java
@@ -5,22 +5,39 @@ import com.techdegree.instateam.service.RoleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
 public class RoleController {
     @Autowired
-    private RoleService mRoleService;
+    private RoleService roleService;
 
     // index of all categories
     @SuppressWarnings("unchecked")
     @RequestMapping("/roles")
     public String listRoles(Model model) {
         // Get all roles
-        List<Role> roles = mRoleService.findAll();
+        List<Role> roles = roleService.findAll();
         model.addAttribute("roles", roles);
         return "role/roles";
+    }
+
+    // Form for adding new role
+    @RequestMapping(value = "/roles",
+        method = RequestMethod.POST)
+    public String addNewRole(@Valid Role role,
+                             BindingResult result,
+                             Model model) {
+        model.addAttribute("role", new Role());
+        if (result.hasErrors()) {
+            return "redirect:/roles";
+        }
+        roleService.save(role);
+        return "redirect:/roles";
     }
 }
