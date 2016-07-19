218917043abcef51ec645c759f6130db00647108
Alexander Nikiforov <nikiforo.alexander@gmail.com>
2016-07-19 16:30:15 +0200

    initial add of model with variables: `id`, `name`, `description` `rolesNeeded`, `collaborators`

---
+++ src/main/java/com/techdegree/instateam/model/Project.java
@@ -0,0 +1,62 @@
+package com.techdegree.instateam.model;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+import java.util.List;
+
+@Entity
+@Table(name = "projects")
+public class Project {
+    @Id
+    @Column(name = "ID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    int id;
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Column(name = "NAME")
+    @NotNull
+    @Pattern(regexp = "\\s*[a-zA-Z0-9]+(\\s+[a-zA-Z0-9]+)*\\s*")
+    String name;
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Column(name = "DESCRIPTION")
+    @NotNull
+    @Size(min = 30)
+    String description;
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @ManyToMany
+    List<Role> rolesNeeded;
+    public void setRolesNeeded(List<Role> rolesNeeded) {
+        this.rolesNeeded = rolesNeeded;
+    }
+    public List<Role> getRolesNeeded() {
+        return rolesNeeded;
+    }
+
+    @ManyToMany
+    List<Collaborator> collaborators;
+    public List<Collaborator> getCollaborators() {
+        return collaborators;
+    }
+    public void setCollaborators(List<Collaborator> collaborators) {
+        this.collaborators = collaborators;
+    }
+}
