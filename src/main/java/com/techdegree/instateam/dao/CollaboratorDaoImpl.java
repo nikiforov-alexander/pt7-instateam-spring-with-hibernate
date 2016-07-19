7f77128ac803dac488b82f0fe0158b1372843d49
Alexander Nikiforov <nikiforo.alexander@gmail.com>
2016-07-19 16:24:12 +0200

    changed `mSessionFactory` to `sessionFactory`. Otherwise it does not work

--- src/main/java/com/techdegree/instateam/dao/CollaboratorDaoImpl.java
+++ src/main/java/com/techdegree/instateam/dao/CollaboratorDaoImpl.java
@@ -4,17 +4,19 @@ import com.techdegree.instateam.model.Collaborator;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public class CollaboratorDaoImpl implements CollaboratorDao {
     @Autowired
-    private SessionFactory mSessionFactory;
+    private SessionFactory sessionFactory;
 
     @Override
     @SuppressWarnings("unchecked")
     public List<Collaborator> findAll() {
-        Session session = mSessionFactory.openSession();
+        Session session = sessionFactory.openSession();
         List<Collaborator> collaborators =
                 session.createCriteria(Collaborator.class).list();
         session.close();
--- src/main/java/com/techdegree/instateam/dao/ProjectDaoImpl.java
+++ src/main/java/com/techdegree/instateam/dao/ProjectDaoImpl.java
@@ -4,17 +4,19 @@ import com.techdegree.instateam.model.Project;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public class ProjectDaoImpl implements ProjectDao {
     @Autowired
-    private SessionFactory mSessionFactory;
+    private SessionFactory sessionFactory;
 
     @Override
     @SuppressWarnings("unchecked")
     public List<Project> findAll() {
-        Session session = mSessionFactory.openSession();
+        Session session = sessionFactory.openSession();
         List<Project> projects = session
                 .createCriteria(Project.class)
                 .list();
