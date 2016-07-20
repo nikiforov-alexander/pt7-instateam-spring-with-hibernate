36c86374ee9ce4608365b490428585bfd76c108d
Alexander Nikiforov <nikiforo.alexander@gmail.com>
2016-07-20 15:12:55 +0200

    optimized imports

--- src/main/java/com/techdegree/instateam/web/controller/RoleController.java
+++ src/main/java/com/techdegree/instateam/web/controller/RoleController.java
@@ -3,14 +3,12 @@ package com.techdegree.instateam.web.controller;
 import com.techdegree.instateam.model.Role;
 import com.techdegree.instateam.service.RoleService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistration;
 
 import javax.validation.Valid;
 import java.util.List;
