a6546257b0f7bf8ddb15de931b4c4a2386547f85
Alexander Nikiforov <nikiforo.alexander@gmail.com>
2016-07-20 14:56:37 +0200

    added `equals` and `hashCode` with intellijidea generators: both two members included

--- src/main/java/com/techdegree/instateam/model/Role.java
+++ src/main/java/com/techdegree/instateam/model/Role.java
@@ -45,4 +45,24 @@ public class Role {
     public Role() {
         // default constructor for JPA
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Role role = (Role) o;
+
+        if (id != role.id) return false;
+        return name != null ? name.equals(role.name) : role.name == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        return result;
+    }
+
 }
