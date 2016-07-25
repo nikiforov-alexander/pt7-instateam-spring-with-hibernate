a7bb7125d4a0a744dc641d45d7462d868bc8e26c
Alexander Nikiforov <nikiforo.alexander@gmail.com>
2016-07-25 11:52:27 +0200

    added roles to `addNewProject` model, and autowired roleService

--- src/main/java/com/techdegree/instateam/web/controller/ProjectController.java
+++ src/main/java/com/techdegree/instateam/web/controller/ProjectController.java
@@ -1,9 +1,10 @@
 package com.techdegree.instateam.web.controller;
 
 import com.techdegree.instateam.model.Project;
+import com.techdegree.instateam.model.Role;
 import com.techdegree.instateam.service.ProjectService;
+import com.techdegree.instateam.service.RoleService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,14 +14,16 @@ import java.util.List;
 @Controller
 public class ProjectController {
     @Autowired
-    private ProjectService mProjectService;
+    private ProjectService projectService;
+    @Autowired
+    private RoleService roleService;
 
     // main page with all projects
     @SuppressWarnings("unchecked")
     @RequestMapping("/")
     public String listProjects(Model model) {
         // Get all projects
-        List<Project> projects = mProjectService.findAll();
+        List<Project> projects = projectService.findAll();
         // add projects to model
         model.addAttribute("projects", projects);
         return "index";
@@ -34,6 +37,9 @@ public class ProjectController {
         // we add action attribute because this template
         // will be re-used for both edit and add new project
         model.addAttribute("action", "add-new");
+        // add roles available
+        List<Role> roles = roleService.findAll();
+        model.addAttribute("roles", roles);
         // if model contains project, e.g. when we
         // user made a mistake, model will be filled with
         // with previously entered data
