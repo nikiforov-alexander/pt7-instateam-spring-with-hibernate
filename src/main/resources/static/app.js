2a1e415fac4556178227fa72bb8f38ffbeb460b1
Nikiforov Alexander <nikiforo.alexander@gmail.com>
2016-07-30 21:22:09 +0200

    removed unneseccary printing in changing `select` color function

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -4,12 +4,8 @@
 // original discussion on stackoverflow:
 // http://stackoverflow.com/questions/15755770/change-text-color-of-selected-option-in-a-select-box
 $("select").change(function(){
-    var origBGColor=$(this).attr("class");
-
     $(this).removeClass($(this).attr('class'))
            .addClass($(":selected",this).attr('class'));
-
-    $("#output").html("original bg color = "+origBGColor+"<br/>Selected BG Color = "+$(":selected",this).attr('class')+"<br/>Current Select BG color = " + $(this).attr('class'));
 });
 
 // if there is an anchor with id="focused-link" we'll focus on it
