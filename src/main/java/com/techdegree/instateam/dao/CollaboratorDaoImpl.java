ce329186ec768a56a8bdfc7618943b53bf969d8c
Alexander Nikiforov <nikiforo.alexander@gmail.com>
2016-08-01 15:37:17 +0200

    Removed repeatable methods, so that only delete is left that is removing links from other tables, before delete

--- src/main/java/com/techdegree/instateam/dao/CollaboratorDaoImpl.java
+++ src/main/java/com/techdegree/instateam/dao/CollaboratorDaoImpl.java
@@ -9,36 +9,9 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public class CollaboratorDaoImpl implements CollaboratorDao {
-    @Autowired
-    private SessionFactory sessionFactory;
-
-    @Override
-    @SuppressWarnings("unchecked")
-    public List<Collaborator> findAll() {
-        Session session = sessionFactory.openSession();
-        List<Collaborator> collaborators =
-                session.createCriteria(Collaborator.class).list();
-        session.close();
-        return collaborators;
-    }
-
-    @Override
-    public Collaborator findById(int id) {
-        Session session = sessionFactory.openSession();
-        Collaborator collaborator = session.get(Collaborator.class, id);
-        session.close();
-        return collaborator;
-    }
-
-    @Override
-    public void save(Collaborator collaborator) {
-        Session session = sessionFactory.openSession();
-        session.beginTransaction();
-        session.saveOrUpdate(collaborator);
-        session.getTransaction().commit();
-        session.close();
-    }
+public class CollaboratorDaoImpl
+        extends GenericDaoImpl<Collaborator>
+        implements CollaboratorDao {
 
     @Override
     public void delete(Collaborator collaborator) {
