5a43762a69260704e794f897092fe30eeae492e8
Nikiforov Alexander <nikiforo.alexander@gmail.com>
2016-08-01 11:47:08 +0200

    added room and comments what to do. "Lunch-commit"

--- src/main/java/com/techdegree/instateam/dao/RoleDaoImpl.java
+++ src/main/java/com/techdegree/instateam/dao/RoleDaoImpl.java
@@ -42,6 +42,7 @@ public class RoleDaoImpl implements RoleDao {
     @Override
     public void delete(Role role) {
         Session session = sessionFactory.openSession();
+
         // update collaborators table by setting foreign key:
         // role_id in collaborators table to null
         session.createSQLQuery(
@@ -49,12 +50,22 @@ public class RoleDaoImpl implements RoleDao {
                         "SET ROLE_ID = NULL " +
                         "WHERE ROLE_ID = " + role.getId())
                 .executeUpdate();
+
         // update projects table by removing links to
         // deleted roles
         session.createSQLQuery(
                 "DELETE FROM PUBLIC.projects_roles " +
                         "WHERE ROLESNEEDED_ID = " + role.getId())
                 .executeUpdate();
+
+        // update projects collaborators table by removing collaborators
+        // that become inaccessible, because the role was deleted
+        // 1. find all collaborators with this roles
+        // SELECT FROM collaborators WHERE role_id = ?
+        // 2. from this collaborators find those who are in projects and delete
+        // DELETE FROM projects_collaborators WHERE collaborator_id = ?
+
+
         // delete role: begin transaction, delete, commit
         session.beginTransaction();
         session.delete(role);
