480b3087181e400123a9cf73f2c9f2d72a999378
Alexander Nikiforov <nikiforo.alexander@gmail.com>
2016-08-01 15:27:45 +0200

    working `RoleDaoImpl` - Hurrah! Now it extends `GenericDaoImpl<Role>`, which means that only delete method is re-implemented by me: the other methods are successfully written by GenericDaoImpl.

--- src/main/java/com/techdegree/instateam/dao/RoleDaoImpl.java
+++ src/main/java/com/techdegree/instateam/dao/RoleDaoImpl.java
@@ -2,42 +2,14 @@ package com.techdegree.instateam.dao;
 
 import com.techdegree.instateam.model.Role;
 import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
-public class RoleDaoImpl implements RoleDao {
-    @Autowired
-    private SessionFactory sessionFactory;
-
-    @Override
-    @SuppressWarnings("unchecked")
-    public List<Role> findAll() {
-        Session session = sessionFactory.openSession();
-        List<Role> roles = session.createCriteria(Role.class).list();
-        session.close();
-        return roles;
-    }
-
-    @Override
-    public void save(Role role) {
-        Session session = sessionFactory.openSession();
-        session.beginTransaction();
-        session.saveOrUpdate(role);
-        session.getTransaction().commit();
-        session.close();
-    }
-
-    @Override
-    public Role findById(int roleId) {
-        Session session = sessionFactory.openSession();
-        Role role = session.get(Role.class, roleId);
-        session.close();
-        return role;
-    }
+public class RoleDaoImpl
+        extends GenericDaoImpl<Role>
+        implements RoleDao {
+    // save, update and findAll() methods are implemented just like
+    // in GenericDaoImpl. Delete method however is different
 
     @Override
     public void delete(Role role) {
