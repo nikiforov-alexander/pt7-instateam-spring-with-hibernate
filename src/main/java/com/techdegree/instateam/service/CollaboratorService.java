d5b75d3406c12a6e5d52a98539f9bb93c2593dff
Alexander Nikiforov <nikiforo.alexander@gmail.com>
2016-08-01 14:26:35 +0200

    made services to extend `GenericService`

--- src/main/java/com/techdegree/instateam/service/CollaboratorService.java
+++ src/main/java/com/techdegree/instateam/service/CollaboratorService.java
@@ -2,11 +2,5 @@ package com.techdegree.instateam.service;
 
 import com.techdegree.instateam.model.Collaborator;
 
-import java.util.List;
-
-public interface CollaboratorService {
-    List<Collaborator> findAll();
-    void save(Collaborator collaborator);
-    void delete(Collaborator collaborator);
-    Collaborator findById(int collaboratorId);
+public interface CollaboratorService extends GenericService<Collaborator> {
 }
