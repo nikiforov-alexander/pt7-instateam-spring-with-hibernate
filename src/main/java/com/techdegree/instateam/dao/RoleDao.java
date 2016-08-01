b1473aa9aabed9663f50573b4eaf24786dee0f74
Nikiforov Alexander <nikiforo.alexander@gmail.com>
2016-08-01 11:33:16 +0200

    made Role extend `GenericDao<T>` that accepts parameter: Object Type <T>. It is working: code is drier

--- src/main/java/com/techdegree/instateam/dao/RoleDao.java
+++ src/main/java/com/techdegree/instateam/dao/RoleDao.java
@@ -2,11 +2,5 @@ package com.techdegree.instateam.dao;
 
 import com.techdegree.instateam.model.Role;
 
-import java.util.List;
-
-public interface RoleDao {
-    List<Role> findAll();
-    void save(Role role);
-    Role findById(int roleId);
-    void delete(Role role);
+public interface RoleDao extends GenericDao<Role> {
 }
