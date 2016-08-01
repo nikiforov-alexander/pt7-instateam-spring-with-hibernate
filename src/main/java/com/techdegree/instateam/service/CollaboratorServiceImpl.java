c887ad7d9e43faf6ac78e00f77488f65ebd07c94
Nikiforov Alexander <nikiforo.alexander@gmail.com>
2016-08-01 20:39:33 +0200

    changed service implementation class so that methods inherited from GenericServiceImpl can be re-used.

--- src/main/java/com/techdegree/instateam/service/CollaboratorServiceImpl.java
+++ src/main/java/com/techdegree/instateam/service/CollaboratorServiceImpl.java
@@ -1,35 +1,29 @@
 package com.techdegree.instateam.service;
 
 import com.techdegree.instateam.dao.CollaboratorDao;
+import com.techdegree.instateam.dao.GenericDao;
 import com.techdegree.instateam.model.Collaborator;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-public class CollaboratorServiceImpl implements CollaboratorService {
-    @Autowired
+public class CollaboratorServiceImpl
+        extends GenericServiceImpl<Collaborator>
+        implements CollaboratorService {
+
     private CollaboratorDao collaboratorDao;
 
-    @Override
-    public List<Collaborator> findAll() {
-       return collaboratorDao.findAll();
-    }
+    public CollaboratorServiceImpl() {
 
-    @Override
-    public void save(Collaborator collaborator) {
-        collaboratorDao.save(collaborator);
     }
-
-    @Override
-    public void delete(Collaborator collaborator) {
-        collaboratorDao.delete(collaborator);
+    @Autowired
+    public CollaboratorServiceImpl(
+            @Qualifier("collaboratorDaoImpl")
+                    GenericDao<Collaborator> genericDao) {
+        super(genericDao);
+        this.collaboratorDao = (CollaboratorDao) genericDao;
     }
-
-    @Override
-    public Collaborator findById(int collaboratorId) {
-        return collaboratorDao.findById(collaboratorId);
-    }
-
 }
