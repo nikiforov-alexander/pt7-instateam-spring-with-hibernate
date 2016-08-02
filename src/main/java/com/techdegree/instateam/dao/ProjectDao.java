78de1796809ba7464250d18231030039afe29474
Alexander Nikiforov <nikiforo.alexander@gmail.com>
2016-08-02 15:10:58 +0200

    added new method on both DAO and Service layer, that initialize only roles' collaborators for only roles that are needed for project

--- src/main/java/com/techdegree/instateam/dao/ProjectDao.java
+++ src/main/java/com/techdegree/instateam/dao/ProjectDao.java
@@ -3,4 +3,5 @@ package com.techdegree.instateam.dao;
 import com.techdegree.instateam.model.Project;
 
 public interface ProjectDao extends GenericDao<Project> {
+    Project findByIdWithRoleCollaboratorsInitialization(int projectId);
 }
--- src/main/java/com/techdegree/instateam/dao/ProjectDaoImpl.java
+++ src/main/java/com/techdegree/instateam/dao/ProjectDaoImpl.java
@@ -1,13 +1,10 @@
 package com.techdegree.instateam.dao;
 
 import com.techdegree.instateam.model.Project;
+import com.techdegree.instateam.model.Role;
 import org.hibernate.Criteria;
+import org.hibernate.Hibernate;
 import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.criterion.DetachedCriteria;
-import org.hibernate.criterion.Projections;
-import org.hibernate.criterion.Subqueries;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -16,7 +13,26 @@ import java.util.List;
 public class ProjectDaoImpl
         extends GenericDaoImpl<Project>
         implements ProjectDao {
-    // "save", "findbyId" methods are implemented in GenericDaoImpl
+    // "save", method are implemented in GenericDaoImpl
+
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public Project findByIdWithRoleCollaboratorsInitialization(int projectId) {
+        Session session = sessionFactory.openSession();
+        Project project = session.get(Project.class, projectId);
+        // lazy instantiate role.collaborators onlly for roles that are needed
+        // for project, because it is needed to
+        // assign rollaborators. When we use eager, we initialize and fetch
+        // all roles.collaborators from database. Now I do not know how to
+        // test this, and whether this approach is better, but that's the best
+        // I can to do here
+        for (Role role : project.getRolesNeeded()) {
+            Hibernate.initialize(role.getCollaborators());
+        }
+        session.close();
+        return project;
+    }
 
     @Override
     @SuppressWarnings("unchecked")
