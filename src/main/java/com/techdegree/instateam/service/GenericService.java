aabc149c5dcb7a2c4b4b07e467be2b6c691c456e
Nikiforov Alexander <nikiforo.alexander@gmail.com>
2016-08-02 19:57:45 +0200

    split `save` into `save`, `update` and `saveOrUpdate`

--- src/main/java/com/techdegree/instateam/service/GenericService.java
+++ src/main/java/com/techdegree/instateam/service/GenericService.java
@@ -5,6 +5,8 @@ import java.util.List;
 public interface GenericService<T> {
     List<T> findAll();
     void save(T object);
+    void update(T object);
+    void saveOrUpdate(T object);
     T findById(int objectId);
     void delete(T object);
 }
