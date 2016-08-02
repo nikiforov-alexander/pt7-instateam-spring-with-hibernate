d038d7021bff2ec84cbfa7c89a439ee22de99ff6
Nikiforov Alexander <nikiforo.alexander@gmail.com>
2016-08-02 20:58:08 +0200

    Overrode the `findAll` method so that it returns sorted list of projects to controller.

--- src/main/java/com/techdegree/instateam/service/ProjectServiceImpl.java
+++ src/main/java/com/techdegree/instateam/service/ProjectServiceImpl.java
@@ -10,6 +10,7 @@ import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Date;
+import java.util.List;
 
 @Service
 public class ProjectServiceImpl
@@ -19,6 +20,24 @@ public class ProjectServiceImpl
 
     public ProjectServiceImpl() {
 
+    }
+
+    // find all method here sorts projects by creation date
+    // before it gets to controller.
+    @SuppressWarnings("unchecked")
+    @Override
+    @Transactional(
+            propagation = Propagation.REQUIRED,
+            readOnly = true)
+    public List<Project> findAll() {
+        List<Project> projects = projectDao.findAll();
+        projects.sort(
+                (p1,p2) ->
+                        p1.getDateCreated().compareTo(p2.getDateCreated())
+        );
+        projects.forEach(project ->
+                System.out.println(project.getDateCreated()));
+        return projects;
     }
 
     // this method is used only when project is created, so that we don't
