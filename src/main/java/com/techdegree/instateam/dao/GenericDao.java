ccab3b2db312e11e65f2b21d9111b3eb0389ddbe
Nikiforov Alexander <nikiforo.alexander@gmail.com>
2016-08-02 19:52:44 +0200

    split `save` to `saveOrUpdate`, `save` and `update`

--- src/main/java/com/techdegree/instateam/dao/GenericDao.java
+++ src/main/java/com/techdegree/instateam/dao/GenericDao.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 public interface GenericDao<T> {
     List<T> findAll();
+    void saveOrUpdate(T object);
     void save(T object);
+    void update(T object);
     T findById(int objectId);
     void delete(T object);
 }
