d02bd22e99a0fbf804b92d1703f61acce5184abd
Nikiforov Alexander <nikiforo.alexander@gmail.com>
2016-08-02 20:01:36 +0200

    added service method implementation with `save` method, which is used when new project is created and sets new Date.

--- src/main/java/com/techdegree/instateam/service/ProjectServiceImpl.java
+++ src/main/java/com/techdegree/instateam/service/ProjectServiceImpl.java
@@ -6,6 +6,10 @@ import com.techdegree.instateam.model.Project;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Date;
 
 @Service
 public class ProjectServiceImpl
@@ -16,6 +20,21 @@ public class ProjectServiceImpl
     public ProjectServiceImpl() {
 
     }
+
+    // this method is used only when project is created, so that we don't
+    // deal with creation date later in updates
+    @Override
+    @Transactional(propagation = Propagation.REQUIRED)
+    public void save(Project project) {
+        // generate Date for project if project is new
+        Date creationDate = new Date();
+        // and set new creation date
+        project.setDateCreated(creationDate);
+        // save new project
+        projectDao.save(project);
+    }
+
+
     @Autowired
     public ProjectServiceImpl(
             @Qualifier("projectDaoImpl")
